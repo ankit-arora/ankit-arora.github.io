f3f20910189beb1ebb4774ecf36c5040aba57204
ankit <ankit.j.arora@gmail.com>
2017-05-19 12:34:33 +0530

    izooto

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+/**
+ * Created by ankit on 17/05/17.
+ */
+var randomnumber=Math.random()*5;importScripts('//cdn.izooto.com/scripts/workers/90b609d3aa8b7d99c138e23f183d1a2352ad562f.js?'+randomnumber);
