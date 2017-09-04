7c2c4bb1db4c7b303f094638b24f1eac55e52096
ankit <ankit.j.arora@gmail.com>
2017-09-04 14:17:55 +0530

    both

--- clevertap_sw.js
+++ clevertap_sw.js
@@ -1,2 +1,1 @@
-importScripts('https://s3-eu-west-1.amazonaws.com/static.wizrocket.com/js/sw_webpush.js?r='+Math.floor(Math.random() * 100) + 1);
-// importScripts('https://ankit-arora.github.io/cw-test.js?v=5');
+importScripts('https://s3-eu-west-1.amazonaws.com/static.wizrocket.com/js/sw_webpush.js');
